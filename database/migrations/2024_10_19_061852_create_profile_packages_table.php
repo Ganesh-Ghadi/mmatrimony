a857a43275ba967a300518bb0e5cd9ce1157f9d3
Vipulkoli2000 <vipulkoli2323@gmail.com>
2024-10-19 18:07:48 +0530

    vv

--- database/migrations/2024_10_19_061852_create_profile_packages_table.php
+++ database/migrations/2024_10_19_061852_create_profile_packages_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,12 +12,12 @@ return new class extends Migration
     {
         Schema::create('profile_packages', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('profile_id')->constrained()->nullable(); // Assuming you have a users table
-            $table->foreignId('package_id')->constrained()->nullable(); // Link to packages
+            $table->foreignId('profile_id')->constrained()->nullable();  // Assuming you have a users table
+            $table->foreignId('package_id')->constrained()->nullable();  // Link to packages
             $table->integer('tokens_received')->default(0);
             $table->integer('tokens_used')->default(0);
-            $table->timestamp('starts_at'); // When the package was bought
-            $table->timestamp('expires_at'); // When the package will expire
+            $table->timestamp('starts_at')->nullable();  // When the package was bought
+            $table->timestamp('expires_at')->nullable();  // When the package will expire
             $table->timestamps();
         });
     }
